70c36f9b8a127e298207bb2bafb642375cd35eb0
SharonMigot <migotsharon@gmail.com>
2020-09-28 17:29:21 +0300

    corrected errors

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -8,7 +8,7 @@ import { Quotes } from '../quotes';
 })
 export class QuotesFormComponent implements OnInit {
 
-  newQuotes = new Quotes(0, "", "", new Date());
+  newQuotes = new Quotes(0,"","",new Date());
   @Output() addQuotes = new EventEmitter<Quotes>();
 
   submitQuotes() {
@@ -23,3 +23,4 @@ export class QuotesFormComponent implements OnInit {
   }
 
 }
+
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -45,3 +45,4 @@ export class QuotesComponent implements OnInit {
 
 }
 
+
