d8fa2385522322a6bb53a3766e4c99a912c16cb2
SharonMigot <migotsharon@gmail.com>
2020-09-28 18:03:37 +0300

    added Quotes

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,12 +9,12 @@ import { Quotes } from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes: Quotes[] = [
-    new Quotes(1, 'Quoted by Winston Churchill.', 'Never bend your head. Always hold it high. Look the world straight in the eye.', new Date(2020, 3, 14)),
-    new Quotes(2, 'Quoted by Mutahi Kagwe', 'If You can get it ,I can get it,anyone can get it', new Date(2020, 6, 9)),
-    new Quotes(3, 'Quoted by Crevance Kate', 'Ignorance and prejudice are fuelling the spread of a preventable disease.  World AIDS Day, 1 December is an opportunity for people worldwide to unite in the fight against HIV and AIDS.', new Date(2013, 1, 12)),
-    new Quotes(4, 'Quoted by  Aggie Cromwell', 'Magic is really very simple, all you’ve got to do is want something and then let yourself have it.', new Date(2019, 4, 18)),
-    new Quotes(5, 'Quoted by Phil Nickinson', 'The future of mobile is the future of online. It is how people access online content now.', new Date(2019, 2, 14)),
-    new Quotes(6, 'Quoted by Ahonen aknon', 'Growth of computer and technology careers is becoming more and more industry-agnostic.', new Date(2030, 3, 14)),
+    new Quotes(1, 'Quote by Walt Disney.', 'The way to get started is to quit talking and begin doing.', new Date(2020, 3, 14)),
+    new Quotes(2, 'Quote by Winston Churchil', 'The pessimist see difficult in every opportunity.The optimist sees opportunity in every difficulty', new Date(2020, 6, 9)),
+    new Quotes(3, 'Quote by Will Rodgers', 'Dont let yesterday take up too much of today.', new Date(2013, 1, 12)),
+    new Quotes(4, 'Quote by OG MANDINO', 'Failure will never overtake me if my determination to succeed is strong enough.', new Date(2019, 4, 18)),
+    new Quotes(5, 'Quote by MAYA ANGELOU', 'We may encounter many defeats but we  must not be defeated.', new Date(2019, 2, 14)),
+    new Quotes(6, 'Quote by Johan Wolfgang', 'Knowing is not enough; we must apply Wishing is not enough;we must do.', new Date(2030, 3, 14)),
   ];
 
   toggleDetails(index) {
